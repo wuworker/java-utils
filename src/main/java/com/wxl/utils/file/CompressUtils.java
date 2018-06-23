9c6e96ec51a4b0cf20e02fe3ae91c91be939725a
wuworker <wuxingle17@163.com>
2018-06-23 09:40:16 +0800

    增加文件处理类

---
+++ src/main/java/com/wxl/utils/file/CompressUtils.java
@@ -0,0 +1,7 @@
+package com.wxl.utils.file.md;
+
+/**
+ * Created by wuxingle on 2018/05/28
+ */
+public class CompressUtils {
+}
---
+++ src/main/java/com/wxl/utils/file/ImageUtils.java
@@ -0,0 +1,7 @@
+package com.wxl.utils.file;
+
+/**
+ * Created by wuxingle on 2018/06/22
+ */
+public class ImageUtils {
+}
